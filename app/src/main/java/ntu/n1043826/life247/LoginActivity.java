40663957a526fc5170860f160310c514096de0aa
ali <basithibnukamil@gmail.com>
2022-03-03 12:07:25 +0000

    Login Screen UI

---
+++ app/src/main/java/ntu/n1043826/life247/LoginActivity.java
@@ -0,0 +1,14 @@
+package ntu.n1043826.life247;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/ntu/n1043826/life247/MainActivity.java
+++ app/src/main/java/ntu/n1043826/life247/MainActivity.java
@@ -2,13 +2,27 @@ package ntu.n1043826.life247;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button letsGetInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        letsGetInButton = findViewById(R.id.startbutton);
+
+        letsGetInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Login Activity
+
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
