a6f60bc7523ce7cac60032ec0f58ca33b832981b
ali <basithibnukamil@gmail.com>
2022-03-03 12:47:10 +0000

    Register page UI and Buttons

--- app/src/main/java/ntu/n1043826/life247/LoginActivity.java
+++ app/src/main/java/ntu/n1043826/life247/LoginActivity.java
@@ -2,13 +2,33 @@ package ntu.n1043826.life247;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+   private Button loginButton;
+   private Button registerButton;
+
+    public LoginActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_login_button);
+        registerButton = findViewById(R.id.register_button_login);
+
+       registerButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               // register button login page activities
+
+               startActivity(new Intent(LoginActivity.this,RegisterAccountActivity.class));
+           }
+       });
     }
 }
